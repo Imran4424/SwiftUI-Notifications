7d21fcc20e9f924f37d2b4b21c7aee7c26a389a5
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-05 13:13:56 +0600

    project init

---
+++ Notifications/NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsApp.swift
+//  Notifications
+//
+//  Created by Shah Md Imran Hossain on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
