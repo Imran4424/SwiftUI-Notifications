8dcfd2ef3dc3cd866d91a41b204c1eb32332dbc6
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-05 13:18:01 +0600

    add  background notification

---
+++ Notifications/BackgroundNotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundNotificationView.swift
+//  Notifications
+//
+//  Created by Shah Md Imran Hossain on 5/11/23.
+//
+
+import SwiftUI
+
+struct BackgroundNotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundNotificationView()
+}
